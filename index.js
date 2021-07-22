013f2bb2e6b7307b6f1d0a4d7d4313538b870f0b
Philip <philip.ramkeerat@indt.org.br>
2021-07-21 23:56:39 -0400

    Implement server listen and attendances route

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => console.log("Server running on port 3000"));
+
+app.get("/attendances", (req, res) => res.send('You are on attendances response'));
