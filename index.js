4c9dcd02fd42ca0c009760fcbbe306595bcc79f8
Philip <philip.ramkeerat@indt.org.br>
2021-07-23 01:03:50 -0400

    Implement tables method for create attendances table

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const customExpress = require("./config/customExpress");
 const connection = require("./infrastructure/connection");
+const Tables = require('./infrastructure/tables')
 
 connection.connect((error) => {
   if (error) {
     console.log(error);
   } else {
     console.log("Connected with success");
+
+    Tables.init(connection)
     const app = customExpress();
+    
     app.listen(3000, () => console.log("Server running on port 3000"));
   }
 });
---
+++ infrastructure/tables.js
@@ -0,0 +1,29 @@
+class Tables {
+  init(connection) {
+    this.connection = connection;
+    
+    this.createAttendances()
+  }
+
+  createAttendances() {
+    const sql = `CREATE TABLE Attendances 
+                 (
+                 id int NOT NULL AUTO_INCREMENT,
+                 CLIENT varchar(50) NOT NULL,
+                 pet varchar(20),
+                 service varchar(20) NOT NULL,
+                 status varchar(20) NOT NULL,
+                 observations text, PRIMARY KEY(id)
+                 )`;
+
+    this.connection.query(sql, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Table Attendances created with success");
+      }
+    });
+  }
+}
+
+module.exports = new Tables();
