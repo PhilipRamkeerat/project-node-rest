1cdb931fa7bb5d9a168538e923b19d07240a06b4
Philip <philip.ramkeerat@indt.org.br>
2021-07-23 19:50:35 -0400

    Implement date validation and user name lenght validation

--- models/attendances.js
+++ models/attendances.js
@@ -1,19 +1,45 @@
 const connection = require("../infrastructure/connection");
-const moment = require('moment')
+const moment = require("moment");
 class Attendance {
   add(attendance, res) {
-    const dateCreate = moment().format('YYYY-MM-DD HH:MM:SS')
-    const dateCurrent = moment(attendance.dateCurrent, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-    const attendanceWithDate = { ...attendance, dateCreate, dateCurrent }
-    const sql = `INSERT INTO Attendances SET ?`;
+    const dateCreate = moment().format("YYYY-MM-DD HH:MM:SS");
+    const dateCurrent = moment(attendance.dateCurrent, "DD/MM/YYYY").format(
+      "YYYY-MM-DD HH:MM:SS"
+    );
 
-    connection.query(sql, attendanceWithDate, (error, response) => {
-      if (error) {
-        res.status(400).json(error);
-      } else {
-        res.status(201).json(response);
-      }
-    });
+    const dateIsValid = moment(dateCurrent).isSameOrAfter(dateCreate);
+    const validClient = attendance.client.length >= 5;
+
+    const validations = [
+      {
+        name: "data",
+        valid: dateIsValid,
+        message: "Date must be greater or equals than current date",
+      },
+      {
+        name: "client",
+        valid: validClient,
+        message: "Customer must be at least five characters long",
+      },
+    ];
+
+    const errors = validations.filter((field) => !field.valid);
+    const existErrors = errors.length;
+
+    if (existErrors) {
+      res.status(400).json(errors);
+    } else {
+      const attendanceWithDate = { ...attendance, dateCreate, dateCurrent };
+      const sql = `INSERT INTO Attendances SET ?`;
+
+      connection.query(sql, attendanceWithDate, (error, response) => {
+        if (error) {
+          res.status(400).json(error);
+        } else {
+          res.status(201).json(response);
+        }
+      });
+    }
   }
 }
 
